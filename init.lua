bfcd07c7765a3ddd5e57b07460b53f22a1b33d32
Zhenbo Li <lizhenbo@yahoo.com>
2024-04-19 21:24:02 -0400

    Import nvim-ufo
    Signed-off-by: Zhenbo Li <lizhenbo@yahoo.com>

--- init.lua
+++ init.lua
@@ -35,3 +35,21 @@ vim.opt.number = true
 
 -- avoids source code shift while waiting for LSP to be loaded
 vim.opt.signcolumn = "yes"
+
+
+-- Fold configuration
+vim.o.foldcolumn = '1' -- '0' is not bad
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+
+-- Option 3: treesitter as a main provider instead
+-- (Note: the `nvim-treesitter` plugin is *not* needed.)
+-- ufo uses the same query files for folding (queries/<lang>/folds.scm)
+-- performance and stability are better than `foldmethod=nvim_treesitter#foldexpr()`
+require('ufo').setup({
+    provider_selector = function(bufnr, filetype, buftype)
+        return {'treesitter', 'indent'}
+    end
+})
+--
---
+++ lua/plugins/nvim_ufo.lua
@@ -0,0 +1,4 @@
+return {
+	{ 'kevinhwang91/promise-async' },
+	{ 'kevinhwang91/nvim-ufo' },
+}
