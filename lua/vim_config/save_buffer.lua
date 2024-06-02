afccffbc2b44562028d98a8e1ae27f6fa267cfa2
Zhenbo Li <lizhenbo@yahoo.com>
2024-06-01 20:19:47 -0400

    Split to other files
    Signed-off-by: Zhenbo Li <lizhenbo@yahoo.com>

---
+++ lua/vim_config/save_buffer.lua
@@ -0,0 +1,14 @@
+
+-- https://stackoverflow.com/a/77180744/1166518
+vim.api.nvim_create_autocmd({"BufWinLeave"}, {
+  pattern = {"*.*"},
+  desc = "save view (folds), when closing file",
+  command = "mkview",
+})
+vim.api.nvim_create_autocmd({"BufWinEnter"}, {
+  pattern = {"*.*"},
+  desc = "load view (folds), when opening file",
+  command = "silent! loadview"
+})
+
+
