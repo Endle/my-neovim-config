c086e865a8d8279e7c416f6256b8a4328f3b06fb
Zhenbo Li <lizhenbo@yahoo.com>
2024-06-25 11:29:22 -0400

    add cscope support

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ vim.keymap.set('n', '<leader>t', ':TagbarToggle<CR>')
 
 
 Ref: https://github.com/Endle/my-vim
+
+
+### cscope  
+(not work) Build db `cscope -bkqR`  
+No recursive query yet. Need to do `Cs db b` in new directories  
+
--- init.lua
+++ init.lua
@@ -24,9 +24,17 @@ vim.opt.number = true
 -- avoids source code shift while waiting for LSP to be loaded
 vim.opt.signcolumn = "yes"
 
+-- tab settings
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+
+
 
 
 -- nnoremap <leader>t :TagbarToggle<CR>
 vim.keymap.set('n', '<leader>t', ':TagbarToggle<CR>')
 
+
+require("cscope_maps").setup()
 
