015fa697f5a3772d9942832eb6774f817959fb19
Zhenbo Li <lizhenbo@yahoo.com>
2024-08-12 19:47:21 -0400

    Rust: Turn off lsp

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -8,7 +8,7 @@ return
 	 build = ":TSUpdate",
 	 config = function()
 			 require("nvim-treesitter.configs").setup {
-			ensure_installed = { "c", "lua", "rust" },
+			ensure_installed = { "c", "lua", "rust", "toml" },
 			highlight = { enable = true, }
 		      }
 		    end
