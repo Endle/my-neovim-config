efd13d615f303f95074fa3d9b5bb3c30edc16582
Zhenbo Li <lizhenbo@yahoo.com>
2024-11-26 14:23:41 -0500

    Move scheme to new file
    Signed-off-by: Zhenbo Li <lizhenbo@yahoo.com>

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,9 +1,9 @@
 return {
-  {
-	  "folke/tokyonight.nvim",
-	  lazy = false,
-	  priority = 1000,
-	  opts = {},
-  },
+  -- {
+  --  "folke/tokyonight.nvim",
+  --  lazy = false,
+  --  priority = 1000,
+  --  opts = {},
+  -- },
   { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} }
 }
