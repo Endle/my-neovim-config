2b8f899b983d2c353869933fa3051146b1114b96
Zhenbo Li <lizhenbo@yahoo.com>
2025-08-03 20:51:27 -0400

    Turn on colour scheme
    Signed-off-by: Zhenbo Li <lizhenbo@yahoo.com>

--- init.lua
+++ init.lua
@@ -1,28 +1,18 @@
-
-
-
-
---vim.cmd[[colorscheme tokyonight-day]]
 vim.opt.number = true
-
-
 -- avoids source code shift while waiting for LSP to be loaded
 vim.opt.signcolumn = "yes"
-
 -- tab settings
 vim.opt.tabstop = 4
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
-
-
-
 
 -- nnoremap <leader>t :TagbarToggle<CR>
 vim.keymap.set('n', '<leader>t', ':TagbarToggle<CR>')
 
 
 require("config.lazy")
-
 require("config.save_buffer")
 require("config.lsp_config")
+
+vim.cmd[[colorscheme tokyonight-day]]
 
